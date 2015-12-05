6f79cce4f113c609750fcc5c4da973aa9821bc2d
buddhaa17 <buddhaa17@gmail.com>
2015-12-06 00:54:32 +0530

    Added singletone design pattern

---
+++ Singleton/LoadBalancerApp/LoadBalancerApp/Loadbalancer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancerApp
+{
+    public class Loadbalancer
+    {
+        private static Loadbalancer _instance;
+
+        private static object syncLock = new object();
+
+        private List<string> _servers = new List<string>();
+
+        private Random _randomGenerator = new Random(); 
+
+        private Loadbalancer()
+        {
+            _servers.Add("ServerI");
+            _servers.Add("ServerII");
+            _servers.Add("ServerIII");
+            _servers.Add("ServerIV");
+            _servers.Add("ServerV");
+        }
+        public static Loadbalancer GetInstance()
+        {
+            if (_instance == null)
+            { 
+                lock(syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Loadbalancer();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public string Server
+        {
+            get 
+            {
+                int count = _randomGenerator.Next(_servers.Count);    
+                return _servers[count];
+            }
+        }
+
+    }
+}
---
+++ Singleton/LoadBalancerApp/LoadBalancerApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Loadbalancer loadBalancer = Loadbalancer.GetInstance();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("For request {0} Loadbalancer assigned {1}",i+1,loadBalancer.Server);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
